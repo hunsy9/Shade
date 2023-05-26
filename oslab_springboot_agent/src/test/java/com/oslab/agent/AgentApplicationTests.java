12f40706c4cbeaad5ce61a34834a9d6e29ef5c5c
juniper0917 <juniper0917@gmail.com>
2023-05-26 16:07:56 +0900

    create spring project

---
+++ oslab_springboot_agent/src/test/java/com/oslab/agent/AgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslab.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
