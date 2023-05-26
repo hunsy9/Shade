eb0093fc213b230a7e0544429eacfad46ae38eed
juniper0917 <juniper0917@gmail.com>
2023-05-26 16:08:39 +0900

    create spring project

---
+++ oslab_springboot_cmanager/src/test/java/com/oslab/cmanager/CmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslab.cmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
