558373fb3db510266107a35488c0d4dfd7f56729
juniper0917 <juniper0917@gmail.com>
2023-08-09 17:47:44 +0900

    pty process secure

--- oslab_node_pty/app.js
+++ oslab_node_pty/app.js
@@ -21,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', ptyControllerRouter);
 app.use('/ptyService', ptyServiceRouter);
 app.use('/users', usersRouter);
--- oslab_node_pty/routes/index.js
+++ oslab_node_pty/routes/index.js
@@ -3,42 +3,74 @@ const os = require("os");
 const pty = require("node-pty");
 let router = express.Router();
 
+let flag = false
+
+const makePty = (shell, setEscape, initCommand) =>{
+  return new Promise((resolve, reject)=>{
+    const ptyTerm =pty.spawn(shell, [], {
+      name: "xterm-color",
+      cols: 100,
+      rows: 24,
+      cwd: process.env.HOME,
+      env: process.env,
+    });
+    ptyTerm.write(setEscape+'\n')
+    ptyTerm.write(initCommand)
+    flag = true
+    resolve(ptyTerm)
+  })
+}
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/startSocketRoom', (req,res)=>{
   const namespace = req.body.nameSpace //nameSpace String
+  const escapeUUID = namespace.split("/")[3]
   console.log(namespace)
   const ssh = io.of(namespace)
 
+  const setEscape = `export PS1=${escapeUUID}`
+
   const initCommand = `sshpass -p ${req.body.password} ssh ${req.body.username}@${req.body.host} -p ${req.body.port}\n`
+
 
   ssh.on("connection", (socket) => {
 
-    socket.on("startChannel", async (identifier) => {
-      console.log(identifier)
+    socket.on("startChannel", async () => {
       console.log("Start ptyChannel ", socket.id);
       const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
 
-      const ptyProcess = pty.spawn(shell, [], {
-        name: "xterm-color",
-        cols: 100,
-        rows: 24,
-        cwd: process.env.HOME,
-        env: process.env,
-      });
+      // const ptyProcess = pty.spawn(shell, [], {
+      //   name: "xterm-color",
+      //   cols: 100,
+      //   rows: 24,
+      //   cwd: process.env.HOME,
+      //   env: process.env,
+      // });
+      //
+      // ptyProcess.write(setEscape+'\n')
+      // ptyProcess.write(initCommand)
 
-      ptyProcess.write(initCommand)
+      await makePty(shell,setEscape,initCommand)
+          .then((ptyProcess)=>{
+            setTimeout(()=>{
+              ptyProcess.onData((e) => {
+                if(e.trim() === escapeUUID){
+                  socket.emit("output", "Connection Closed!");
+                  socket.emit("unexpectedExit")
+                  socket.disconnect()
+                }
+                console.log("data: " + e)
+                socket.emit("output", e);
+              });
 
-      ptyProcess.onData((e) => {
-        console.log("data: " + e)
-        socket.emit("output", e);
-      });
-
-      socket.on("input", (input) => {
-        ptyProcess.write(input);
-      });
+              socket.on("input", (input) => {
+                ptyProcess.write(input);
+              });
+            },300)
+          })
     })
 
     socket.on("disconnect", () => {
