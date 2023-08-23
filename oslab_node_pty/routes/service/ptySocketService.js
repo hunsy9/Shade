5ab60ce6dd7a613e5e453e7a399db87ccf223bd9
juniper0917 <juniper0917@gmail.com>
2023-08-24 00:54:45 +0900

    cors

--- oslab_node_pty/routes/service/ptySocketService.js
+++ oslab_node_pty/routes/service/ptySocketService.js
@@ -15,7 +15,8 @@ httpServer.listen(port, () => {
 
     const io = new PtySocketService(httpServer, {
         cors:{
-            origin:true,
+            origin: "*",
+            methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
         }
     });
 
