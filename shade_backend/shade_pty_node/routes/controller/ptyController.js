0eea8bcd8ef359974f93db4366af9f27aac2b464
hunsy9 <juniper0917@gmail.com>
2024-03-17 14:30:23 +0900

    refactor: 불필요한 console.log 제거

--- shade_backend/shade_pty_node/routes/controller/ptyController.js
+++ shade_backend/shade_pty_node/routes/controller/ptyController.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const { socketIO } = require('../service/ptySocketService')
 
 router.post('makePtySocketNameSpace',(req,res)=>{
-    console.log("들어옴")
     const io = socketIO
-    console.log("aad")
     const namespace = req.body //nameSpace String
-    console.log("adfad")
     const room = io.of(namespace)
     console.log(room)
 })
